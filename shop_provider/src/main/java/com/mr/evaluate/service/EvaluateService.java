681f20c8cb010f2cec832c986e649be9c941ab8e
wgz <1048909052@qq.com>
2019-05-08 17:14:05 +0800

    更新

--- shop_provider/src/main/java/com/mr/evaluate/service/EvaluateService.java
+++ shop_provider/src/main/java/com/mr/evaluate/service/EvaluateService.java
@@ -5,9 +5,15 @@ import com.mr.pojo.Evaluate;
 import com.mr.utils.Page;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * Created by shun on 2019/5/8.
  */
 public interface EvaluateService {
     PageInfo<Evaluate> queryEvaluateList(Page page);
+
+    List selectl();
+
+    List selectddd();
 }
