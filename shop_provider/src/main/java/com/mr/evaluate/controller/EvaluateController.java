2ee6d8d6d996c4c5e354c4d2cbb4a0d70f36e8ed
wgz <1048909052@qq.com>
2019-05-08 17:13:06 +0800

    更新

--- shop_provider/src/main/java/com/mr/evaluate/controller/EvaluateController.java
+++ shop_provider/src/main/java/com/mr/evaluate/controller/EvaluateController.java
@@ -8,8 +8,11 @@ import com.mr.utils.DataGridVo;
 import com.mr.utils.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by shun on 2019/5/8.
@@ -33,6 +36,25 @@ public class EvaluateController {
         DataGridVo<Evaluate> dataGridVo = new DataGridVo<Evaluate>(0,"",pageInfo.getTotal(),pageInfo.getList());
         return dataGridVo;
     }
+    /**
+     * 查询
+     * @return
+     */
+    @RequestMapping("selectEva")
+    @ResponseBody
+    public List selectl(){
+        List lll =evaluateService.selectl();
+        return lll;
+    }
 
+    /**
+     * 查询
+     * @return
+     */
+    @RequestMapping("selectddd")
+    public List selectddd(){
+        List list = evaluateService.selectddd();
+        return list;
+    }
 
 }
