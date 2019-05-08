835d947c12a87b97b947f80adba5a19e5d983460
wgz <1048909052@qq.com>
2019-05-08 17:18:47 +0800

    更新

--- shop_consumer_feign/src/main/java/com/mr/user/service/UserService.java
+++ shop_consumer_feign/src/main/java/com/mr/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.mr.user.service;
 
+import com.mr.pojo.Address;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,4 +17,6 @@ public interface UserService {
 
     @RequestMapping(value = "/queryComName",method = RequestMethod.GET)
     List queryComName(@RequestParam("comName") String comName);
+
+
 }
