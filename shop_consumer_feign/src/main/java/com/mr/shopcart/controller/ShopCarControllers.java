149dc4e8b2b68b921ee916330bc5fafc9e9cc92a
dhs <13853761725@163.com>
2019-05-09 10:36:21 +0800

    更新

---
+++ shop_consumer_feign/src/main/java/com/mr/shopcart/controller/ShopCarControllers.java
@@ -0,0 +1,33 @@
+package com.mr.shopcart.controller;
+
+import com.mr.shopcart.pojo.ResponCar;
+import com.mr.shopcart.service.ShopServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created by 1115393087 on 2019/4/29.
+ */
+@RestController
+public class ShopCarControllers {
+
+    @Autowired
+    private ShopServices shopService;
+
+
+    @RequestMapping(value = "shopcartMain",produces = "application/json")
+    public ModelAndView shopcartMain1(ResponCar responCar){
+        ModelAndView mo = new ModelAndView();
+        List list = shopService.shopcartMain1(responCar);
+        mo.addObject("list",list);
+        mo.setViewName("/shopcart/shopcart");
+        return  mo;
+    }
+
+
+
+}
---
+++ shop_consumer_feign/src/main/java/com/mr/shopcart/service/ShopServices.java
@@ -0,0 +1,22 @@
+package com.mr.shopcart.service;
+
+import com.mr.shopcart.pojo.ResponCar;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created by 1115393087 on 2019/4/30.
+ */
+@FeignClient("PROVIDER-SHOP")
+@Service
+public interface ShopServices {
+
+
+    @RequestMapping(value = "/shopcartMain1",method = RequestMethod.GET)
+    List shopcartMain1(@RequestBody ResponCar responCar);
+}
